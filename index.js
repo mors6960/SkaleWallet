fb886b62a06ad4260e01ac191742200dd18cb9a0
Shubham Mor <160603111+mors6960@users.noreply.github.com>
2024-07-26 16:54:38 +0530

    second

---
+++ index.js
@@ -0,0 +1,13 @@
+const TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
+
+// Replace with your bot token
+const bot = new TelegramBot('7402148369:AAGCAP0VbA9keqjpOzu3whYL98nDVf3OeFM', { polling: true });
+
+// Command to connect wallet
+bot.onText(/\/connect/, async (msg) => {
+    const chatId = msg.chat.id;
+    // Send a link to connect wallet through MetaMask
+    const connectLink = 'https://mors6960.github.io/SkaleBlockchain/'; 
+    bot.sendMessage(chatId, `Please connect your wallet using the following link: ${connectLink}`);
+});
