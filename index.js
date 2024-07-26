9ba372ab894c85ca2dd0010fe342e97a1028b307
Shubham Mor <160603111+mors6960@users.noreply.github.com>
2024-07-26 23:47:18 +0530

    third

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const TelegramBot = require('node-telegram-bot-api');
-const axios = require('axios');
+const botToken = '7402148369:AAGCAP0VbA9keqjpOzu3whYL98nDVf3OeFM';
 
-// Replace with your bot token
-const bot = new TelegramBot('7402148369:AAGCAP0VbA9keqjpOzu3whYL98nDVf3OeFM', { polling: true });
+const bot = new TelegramBot(botToken, { polling: true });
 
-// Command to connect wallet
-bot.onText(/\/connect/, async (msg) => {
+const dappUrl = 'https://mors6960.github.io/SkaleWallet/'; // Replace with the URL where your HTML is hosted
+
+bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    // Send a link to connect wallet through MetaMask
-    const connectLink = 'https://mors6960.github.io/SkaleWallet/'; 
-    bot.sendMessage(chatId, `Please connect your wallet using the following link: ${connectLink}`);
+    bot.sendMessage(chatId, 'Welcome! Click the link below to connect your wallet:', {
+        reply_markup: {
+            inline_keyboard: [[{ text: 'Connect Wallet', url: dappUrl }]]
+        }
+    });
 });
 
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    if (msg.text.toLowerCase() === 'connect') {
+        bot.sendMessage(chatId, 'Click the link below to connect your wallet:', {
+            reply_markup: {
+                inline_keyboard: [[{ text: 'Connect Wallet', url: dappUrl }]]
+            }
+        });
+    } else {
+        bot.sendMessage(chatId, 'Send /start to begin.');
+    }
+});
 console.log ("Bot is running....")
